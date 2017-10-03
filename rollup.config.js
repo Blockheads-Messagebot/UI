7b0c20bdf7f3dec2e50c2991ea52b8300773c6d2
Bibliofile <Bibliofilegit@gmail.com>
2017-10-02 20:55:42 -0600

    Bump version - include build script

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import sass from 'rollup-plugin-sass'
+import typescript from 'rollup-plugin-typescript2'
+import string from 'rollup-plugin-string'
+
+import { unlinkSync, existsSync } from 'fs'
+// Avoid build errors, unfortunately doesn't fix --watch
+for (let name of ['index.d.ts', 'polyfill.d.ts', 'api.d.ts']) {
+    if (existsSync(`${__dirname}/${name}`)) unlinkSync(`${__dirname}/${name}`)
+}
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        file: 'bundle.js',
+        format: 'es'
+    },
+    plugins: [
+        sass({ output: 'bundle.css', options: { outputStyle: 'compressed' } }),
+        typescript(),
+        string({ include: '**/*.html'})
+    ],
+    external: [
+        '@bhmb/bot',
+    ],
+    globals: {
+        '@bhmb/bot': '@bhmb/bot'
+    }
+};
